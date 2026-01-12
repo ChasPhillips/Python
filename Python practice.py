bcc3e0499e28e896715e55ef7dd5e0f83aa64ccf
ChasPhillips <chaselliottphillips@gmail.com>
2026-01-11 21:03:15 -0600

    Python Practice

---
+++ Python practice.py
@@ -0,0 +1,10 @@
+for i in range(10):
+    print("Hello, world!")
+
+print("Hello, how are you?")
+
+print("Today was a good day!")
+
+lions = ['Simba', 'Mufasa', 'Scar']
+for lion in lions:
+    print("The lion name is " + lion)
